73c4ab5479fb9a801f0318b90bb24b9796ec966c
KhaledAbdelhak <khaled.abdelhak@hotmail.com>
2021-11-27 16:19:13 +0100

    refactorisation de l'application

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,8 +5,16 @@ import MovieDetails from "../movie-details"
 import './style.css'
 
 class App extends Component {
-  render() {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      selectedMovie: 0
+    }
+  }
+
+  render() {
     return (
       <div className="App d-flex flex-column">
         <Header />
--- src/components/movies/movie/index.js
+++ src/components/movies/movie/index.js
@@ -3,12 +3,13 @@ import React, { Component } from 'react';
 export default class Movie extends Component {
     render() {
         return (
-            <div className="w-25 p-2">
-                <div  className="card">
-                    <img alt="" src="https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg" className="card-img-top" />
-                    <div className="card-body">
-                        <h5 className="card-title">Gladiator</h5>
-                        <p className="card-text">Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire avec une bravoure et un dévouement exemplaires.</p>
+            <div className="w-50 p-2">
+                <div  className="border d-flex">
+                    <img width="150" height="200" alt="film" src="https://www.gstatic.com/tv/thumb/v22vodart/24674/p24674_v_v8_ag.jpg" className="" />
+                    <div className="flex-fill d-flex flex-column p-3">
+                        <h5>titre</h5>
+                        <hr className="w-100" />
+                        <p >Le général romain Maximus est le plus fidèle soutien de l'empereur Marc Aurèle, qu'il a conduit de victoire en victoire avec une bravoure et un dévouement exemplaires.</p>
                     </div>
                 </div>
             </div>  
