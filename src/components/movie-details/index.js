98ae7029dd5e92f4082558eea75320bfbb1d4543
KhaledAbdelhak <khaled.abdelhak@hotmail.com>
2021-11-27 16:38:24 +0100

    mis en place des détails d'un film

--- src/components/movie-details/index.js
+++ src/components/movie-details/index.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react';
 export default class MovieDetails extends Component {
     render() {
         return (
-            <div className="w-25 border p-4">Movie Details</div>
+            <div className="w-25 border p-4 d-flex flex-column">
+                <h5 className="mx-auto">{this.props.movie.title}</h5>
+                <hr className="w-100" />
+                <div>
+                    <img className="d-block mx-auto" src={this.props.movie.img} />  
+                </div>
+                <span className="text-secondary">{this.props.movie.details}</span>
+                <hr className="w-100" />
+                <span>{this.props.movie.description}</span>
+            </div>
         )
     }
 }
